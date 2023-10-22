28e7d72256d8cd2b44de4267fcd78d1f3a4043f0
gulata <gul.ataalp@gmail.com>
2023-10-22 12:05:55 +0300

    22ekim

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -56,10 +56,10 @@ public class Driver {
                     WebDriverManager.chromedriver().setup();
                     driver=new ChromeDriver();
             }
-
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         }
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
         return driver;
 
 
